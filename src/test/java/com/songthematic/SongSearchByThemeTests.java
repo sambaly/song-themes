481a621650efcb486744a0ad5282548d47ad6788
sambaly <kote.sambaly@gmail.com>
2023-12-28 17:24:13 +0000

    Properly handled case insensitive theme search.

--- src/test/java/com/songthematic/SongSearchByThemeTests.java
+++ src/test/java/com/songthematic/SongSearchByThemeTests.java
@@ -2,7 +2,12 @@ package com.songthematic;
 
 import com.songthematic.songthemes.Song;
 import com.songthematic.songthemes.SongSearcher;
+import com.songthematic.songthemes.SongView;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
@@ -12,7 +17,7 @@ public class SongSearchByThemeTests {
 
     @Test
     void searchForThemeThatDoesNotExistReturnsNoResults() {
-        SongSearcher songSearcher = SongSearcher.withSongsForTheme("New Years");
+        SongSearcher songSearcher = SongSearcher.withOneSongForTheme("New Years");
 
         List<String> foundSongs = songSearcher.byTheme("AppleSauce");
 
@@ -20,13 +25,33 @@ public class SongSearchByThemeTests {
                 .isEmpty();
     }
 
-    @Test
-    void searchForThemeFindsOneMatchingSong() {
-        SongSearcher songSearcher = SongSearcher.createSongSearcher(new Song("New Years", "auld lang syne"));
+    @ParameterizedTest()
+    @CsvSource({
+            "new years, New Years",
+            "new years, new years",
+            "New Years, New Years",
+            "New Years, new years",
+    })
+    void searchForThemeFindsOneMatchingSong(String songTheme, String requestedTheme) {
+        SongSearcher songSearcher = SongSearcher.createSongSearcher(new Song(songTheme, "auld lang syne"));
 
-        List<String> foundSong = songSearcher.byTheme("New Years");
+        List<String> foundSong = songSearcher.byTheme(requestedTheme);
 
         assertThat(foundSong)
                 .containsExactly("auld lang syne");
+    }
+
+    @Test
+    @Disabled
+    void searchForThemeFindsMultipleMatchingSongs() {
+        SongSearcher songSearcher = SongSearcher.createSongSearcher(
+                new Song("New Years", "auld lang syne"),
+                new Song("New years", "New Year's Eve In A Haunted House"));
+
+        List<String> foundSongs = songSearcher.byTheme("New Years");
+
+        assertThat(foundSongs)
+                .containsExactly("auld lang syne", "New Year's Eve In A Haunted House");
+
     }
 }
