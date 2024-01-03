fe68b96b4640cc3e0baa6e9acf47f7aa106a8b90
sambaly <kote.sambaly@gmail.com>
2024-01-03 09:35:34 +0000

    Renamed application class to startup

--- src/main/java/com/songthematic/ServletInitializer.java
+++ src/main/java/com/songthematic/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(SongThemesApplication.class);
+		return application.sources(SongThemesStartup.class);
 	}
 
 }
