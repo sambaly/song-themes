7f120b68cf0fee03039b2cc7b1244c1186102dc7
sambaly <kote.sambaly@gmail.com>
2024-01-29 14:12:37 +0000

    Prepare refactoring

--- src/main/java/com/songthematic/songthemes/application/TsvSongParser.java
+++ src/main/java/com/songthematic/songthemes/application/TsvSongParser.java
@@ -2,13 +2,27 @@ package com.songthematic.songthemes.application;
 
 import com.songthematic.songthemes.domain.Song;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class TsvSongParser {
     public List<Song> parse(String tsvSongs) {
         String[] columns = tsvSongs.split("\t");
-        Song song = new Song(columns[0], ", ", "", "", Collections.emptyList());
+        String artist = columns[0];
+        String songTitle = columns[1];
+        String releaseTitle = columns[2];
+        String releaseType = columns[3];
+        List<String> themes = parseThemes(columns);
+        Song song = new Song(artist, songTitle, releaseTitle, releaseType, themes);
         return List.of(song);
+    }
+
+    private List<String> parseThemes(String[] columns) {
+        ArrayList<String> themes = new ArrayList<>();
+
+        for (int i = 5; i <= 8; i++) {
+            themes.add(columns[i]);
+        }
+        return themes;
     }
 }
--- src/test/java/com/songthematic/songthemes/application/TsvSongParserTest.java
+++ src/test/java/com/songthematic/songthemes/application/TsvSongParserTest.java
@@ -1,6 +1,7 @@
 package com.songthematic.songthemes.application;
 
 import com.songthematic.songthemes.domain.Song;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -11,14 +12,26 @@ class TsvSongParserTest {
 
     @Test
     void parseSongFromTabSeparatedValues() {
-        String csvSongs = "Earth, Wind & Fire\tGratitude\tThank You\tThanks Gratitude\tRizzi";
+        String csvSongs = "Earth, Wind & Fire\tGratitude\tReleaseTitle\tReleaseType\tNotes\tThank You\tThanks\tGratitude\tTheme4\tRizzi";
 
         TsvSongParser tsvSongParser = new TsvSongParser();
-
         List<Song> songs = tsvSongParser.parse(csvSongs);
 
         assertThat(songs)
-                .extracting(Song::artist)
-                .containsExactly("Earth, Wind & Fire");
+                .containsExactly(new Song("Earth, Wind & Fire", "Gratitude",
+                        "ReleaseTitle", "ReleaseType", List.of("Thank You", "Thanks", "Gratitude", "Theme4")));
+    }
+
+    @Test
+    @Disabled
+    void parseSongWithOnlyOneThemeHasOneTheme() {
+        String csvSongs = "DontCareArtist\tDontCareSongTitle\tDontCareReleaseTitle\tDontCareReleaseType\tNotes\tThank You\t\t\tDontCareContributor";
+
+        TsvSongParser tsvSongParser = new TsvSongParser();
+        List<Song> songs = tsvSongParser.parse(csvSongs);
+
+        assertThat(songs)
+                .containsExactly(new Song("DontCareArtist", "DontCareSongTitle",
+                        "DontCareReleaseTitle", "DontCareReleaseType", List.of("Thank You")));
     }
 }
